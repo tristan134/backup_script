9f260b80f660b513a8f55ff6d47af4568906796d
Tristan <77080729+tristan134@users.noreply.github.com>
2024-02-08 23:10:43 +0100

    Update backup.sh
    Big update
    The script is now separated in different functions. It will also check, if all required packages are installed and if not it will install them automatically. I implemented also more secure features so that the script doesn't fail too much

--- backup.sh
+++ backup.sh
@@ -1,51 +1,85 @@
 #!/bin/bash
 
 # Configure NAS
-nas_ip="192.168.1.xxx"
+nas_ip="192.168.178.210"
 
 # Directory to be copied
-source_dir="/home/user/Documents"
+source_dir="/home/data"
 
-# Temporary directory
-temp_dir="/home/user/temp"
+# Temporary directory.
+# It will be also the name of the copied folder on the NAS
+# Name it wisely like /Backup- the dash is for the date that will be append to the folder name
+temp_dir="/home/Backup-"
 
 # Directory on the NAS
-target_dir="/volume1/Backups/"
+target_dir="/volume1/Backups/Test"
 
-# Mountpoint on the PC (the directory must exist!)
-mount_point="/home/user/mount"
+# Mountpoint on the System (the directory must exist!)
+mount_point="/home/mount"
 
 # Variable for the Date
 date=$(date +%Y-%m-%d)
 
+check_packages(){
+  if [ "$(dpkg -l | awk '/nfs-common/ {print }'|wc -l)" -ge 1 ]; then
+    echo ""
+  else
+    echo "nfs-common package is missing. It will be installed automatically"
+    apt-get install nfs-common -y >/dev/null 2>&1
+  fi
+}
 
-if ping -c 1 $nas_ip &> /dev/null
-then
-  sleep 3
-  echo
-  echo "NAS reachable"
-  echo
+create_directory(){
   echo "Create a temporary directory and copy files there"
   sudo mkdir $temp_dir$date
   sudo cp -r $source_dir $temp_dir$date
   echo "Directory has been created, and files have been copied"
-  echo
-  sudo mount -t nfs $nas_ip:$target_dir $mount_point
-  echo "NAS has been mounted"
-  echo
+}
+
+copy_files(){
   echo "Files are being copied..."
   sudo cp -r $temp_dir$date $mount_point
-  echo
   echo "Files have been successfully copied"
-  echo
+}
+
+clean_up(){
   sudo umount $mount_point
   echo "NAS has been unmounted"
-  echo
   echo "Delete temporary directory"
   sudo rm -r $temp_dir$date
-  echo
   echo "All done, script terminated"
+}
 
+mount_nas(){
+sudo mount -t nfs $nas_ip:$target_dir $mount_point
+if mountpoint -q $mount_point; then
+  echo "$mount_point is mounted"
+	echo
+	create_directory
+	echo
+	copy_files
+	echo
+	clean_up
+    else
+        echo "NAS could not be mounted. Abort script."
+  fi
+}
+
+main(){
+check_packages
+echo "Check if NAS is reachable"
+if ping -c 1 $nas_ip &> /dev/null
+then
+  sleep 3
+  echo
+  echo "NAS is reachable"
+  echo
+  mount_nas
 else
   echo "NAS not reachable, files were not backed up!"
+fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
 fi
